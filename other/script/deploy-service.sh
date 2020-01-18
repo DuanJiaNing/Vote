d09e123b1baac243b922c30d5c659e75b2e66ad8
DuanJiaNing <aimeimeits@gmail.com>
2020-01-18 23:17:34 +0800

    deploy

---
+++ other/script/deploy-service.sh
@@ -0,0 +1,16 @@
+NAME=vote-service-impl-1.0-SNAPSHOT.jar
+echo $NAME
+ID=`ps -ef | grep "$NAME" | grep -v "grep" | awk '{print $2}'`
+echo $ID
+echo "---------------"
+for id in $ID
+do
+	kill -9 $id
+	echo "killed $id"
+done
+echo "prepare to deploy $NAME ..."
+
+java -Xmx256m -jar /usr/duan/jenkins/vote/jar/vote-service-impl-1.0-SNAPSHOT.jar > /usr/duan/jenkins/vote/log/vote-service.log 2>&1 &
+java -Xmx256m -jar /usr/duan/vote/vote-service-impl-1.0-SNAPSHOT.jar > /usr/duan/vote/vote-service.log 2>&1 &
+
+echo excute success from deploy-service.sh
