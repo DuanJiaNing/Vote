7568c6c4a9323f9404b561ce6ef2ff8712c17688
DuanJiaNing <aimeimeits@gmail.com>
2020-02-09 21:55:06 +0800

    getSummary

--- vote-api/src/main/java/com/duan/vote/api/CommentController.java
+++ vote-api/src/main/java/com/duan/vote/api/CommentController.java
@@ -3,11 +3,8 @@ package com.duan.vote.api;
 import com.duan.vote.common.ResultModel;
 import com.duan.vote.config.Config;
 import com.duan.vote.dto.CommentDTO;
-import com.duan.vote.dto.UserDTO;
 import com.duan.vote.service.CommentService;
 import com.duan.vote.service.UserService;
-import com.duan.vote.utils.ResultUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -32,6 +29,6 @@ public class CommentController {
 
     @PostMapping()
     public ResultModel<CommentDTO> add(@RequestBody CommentDTO dto, @RequestHeader("uid") String uid) {
-
+        return null;
     }
 }
